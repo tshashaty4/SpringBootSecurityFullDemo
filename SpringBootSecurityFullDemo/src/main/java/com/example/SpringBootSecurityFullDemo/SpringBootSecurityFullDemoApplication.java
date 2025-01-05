d509e024d6288e677fd6ba026fd018c2f0fcd00e
tshas <tshas@LAPTOP-CCSTROFR>
2025-01-05 15:29:33 -0500

    start

---
+++ SpringBootSecurityFullDemo/src/main/java/com/example/SpringBootSecurityFullDemo/SpringBootSecurityFullDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringBootSecurityFullDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSecurityFullDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSecurityFullDemoApplication.class, args);
+	}
+
+}
